f1f5c4a1da4dcaa79b5117ad7e7254bc13b3725c
Balasaheb thakare <balasaheb.thakare@enrichai.com>
2021-11-26 04:17:09 +0530

    added tables with shimmer in hoem compoent

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MatTableModule } from '@angular/material';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import {MatButtonModule} from '@angular/material/button';
 
+const homeRoutes: Routes= [
+  {
+    path: '',
+    component: HomeComponent,
+  }
+]
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatTableModule,
+    NgxSkeletonLoaderModule,
+    RouterModule.forChild(homeRoutes)
   ]
 })
 export class HomeModule { }
--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort, MatTableDataSource } from '@angular/material';
+import { BehaviorSubject, ReplaySubject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { MoviesSService } from 'src/app/movies-s.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private movieService: MoviesSService) { }
+
+  loading1:BehaviorSubject<boolean> =  new BehaviorSubject(true);
+  loading1$ = this.loading1.asObservable();
+
+  loading2: BehaviorSubject<boolean> =  new BehaviorSubject(true);
+  loading2$ = this.loading2.asObservable();
+
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+
+  displayComingSoonMovies = ['title', 'year', 'genres', 'stars', 'releaseState', 'directors'];
+  displayInTheaterSoonMovies = ['title', 'year', 'genres', 'stars', 'releaseState', 'directors'];
+
+  public dataSource = new MatTableDataSource<any>();
+  public dataSource2 = new MatTableDataSource<any>();
+
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
 
   ngOnInit() {
+    this.getComingSoonMovie();
+    this.getInTheaterMovie();
   }
+
+  getComingSoonMovie() {
+    this.movieService.getComingSoonMovies$.pipe(takeUntil(this.destroyed$)).subscribe(
+      reqObj => { this.dataSource.data = reqObj.items; this.loading1.next(false) }
+    );
+  }
+
+  getInTheaterMovie() {
+    this.movieService.getInTheaterMovies$.pipe(takeUntil(this.destroyed$)).subscribe(
+      reqObj => { this.dataSource2.data = reqObj.items; this.loading2.next(false) }
+    );
+  }
+
+  refreshComingSoon(){
+    this.dataSource.data = [];
+    this.loading1.next(false);
+    this.getComingSoonMovie();
+  }
+
+  refreshTheaterMovie(){
+    this.dataSource2.data = [];
+    this.loading2.next(false);
+    this.getInTheaterMovie();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next();
+    this.destroyed$.complete
+  }
+
 
 }
